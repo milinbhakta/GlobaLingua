4745e02c4645b39718fd48ddbb9fe1cc858fc457
Milinbhakta <desire2dosomething@gmail.com>
2024-02-09 20:39:16 -0400

    Refactor Header component imports and remove MenuIcon

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,5 @@
-import {
-  Toolbar,
-  IconButton,
-  Typography,
-  MenuItem,
-  Select,
-} from "@mui/material";
+import { Toolbar, Typography, MenuItem, Select } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
-import MenuIcon from "@mui/icons-material/Menu";
 import { SetStateAction } from "react";
 import { Locales } from "../i18n/i18n-types";
 import { locales } from "../i18n/i18n-util";
@@ -29,21 +22,18 @@ function Header() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           {LL.APPNAME()}
         </Typography>
         <Select value={locale || ""} onChange={handleLanguageChange}>
-            <MenuItem value="" disabled>
-                {LL.CHOOSE_LOCALE()}
+          <MenuItem value="" disabled>
+            {LL.CHOOSE_LOCALE()}
+          </MenuItem>
+          {locales.map((locale) => (
+            <MenuItem key={locale} value={locale}>
+              {locale} | {getLocalName(locale)}
             </MenuItem>
-            {locales.map((locale) => (
-                <MenuItem key={locale} value={locale}>
-                    {locale} | {getLocalName(locale)}
-                </MenuItem>
-            ))}
+          ))}
         </Select>
       </Toolbar>
     </AppBar>
